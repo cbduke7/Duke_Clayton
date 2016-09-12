80b40aaa05feb68c4ccb029b7aee39c281e199b9
cbduke7 <cbduke14@yahoo.com>
2016-09-12 10:14:58 -0700

    finished file

--- PyLesson_03/ex_o3.py
+++ PyLesson_03/ex_o3.py
@@ -1,2 +1,11 @@
 height = int(input("Please enter how many inches you are over five feet:"))
+Boykgbyh = (1.9 * height)
+Boyidealkg = (52 + Boykgbyh)
+Boykgtolbs = (Boyidealkg * 2.20462)
+
+Girlkgbyh = (1.7 * height)
+Girlidealkg = (49 + Girlkgbyh)
+Girlkgtolbs = (Girlidealkg * 2.20462)
+print("Your ideal weight if you're a boy is" ,Boykgtolbs, "lbs. If you are a girl it is",Girlkgtolbs,"lbs.")
+
  
