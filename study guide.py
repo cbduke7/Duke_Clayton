8212fb8ec3072be66474676c88a1ba210285752f
cbduke7 <cbduke14@yahoo.com>
2016-09-13 10:24:10 -0700

    added file

---
+++ study guide.py
@@ -0,0 +1,18 @@
+num = int(input("How many weapons does iron man have in his suit?"))
+print("IronMan has" ,num, "kinds of weapons in his suit.")
+
+pres = "Abaraham Lincoln"
+quote = "Whatever you are, be a good one."
+print(pres, "once said, \"" ,quote, "\"")
+
+h = 45
+w = 64
+area = (h * w)
+print("The area of your rectangle is..." ,area)
+
+
+print("This one goes on top\nThis one goes on bottom")
+
+
+
+print("a\tb\tc\nd\te\tf\ng\th\ti")
