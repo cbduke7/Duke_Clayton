cb99d61cc21771f45ddc3b42620941d5117a8ec9
cbduke7 <cbduke14@yahoo.com>
2016-09-20 10:18:16 -0700

    almost finished files

--- PyLesson_04/Ex_01Receipt.py
+++ PyLesson_04/Ex_01Receipt.py
@@ -1,26 +1,21 @@
+def format(item, price):
+    print("{*:<1}{:^15}\t{:10.2f}".format(item, price))
+
 item1 = input("Please enter item 1:")
-price1 = int(input("Please enter the price:"))
+price1 = float(input("Please enter the price:"))
 item2 = input("Please enter item 2:")
-price2 = int(input("Please enter the price:"))
+price2 = float(input("Please enter the price:"))
 item3 = input("Please enter item 3:")
-price3 = int(input("Please enter the price:"))
+price3 = float(input("Please enter the price:"))
 
-def printf(word, number):
-    print("{:<20}\t{:10.2f}".format(word, number))
+subtotal = (price1 + price2 + price3)
+tax = (subtotal * .075)
+total = (subtotal + tax)
 
-print("<<<<<<<<<<<<__Receipt__>>>>>>>>>>>>")
-
-   
-word = item1
-number = price1
-
-printf(word, number)
-
-word = item2
-number = price2
-
-printf(word, number)
-
-word = item3
-number = price3
-
+print("<<<<<<<receipt>>>>>>")
+format(item1, price1)
+format(item2, price2)
+format(item3, price3)
+format("Subtotal: ", subtotal)
+format("Tax: ")
+format("Total: ")
