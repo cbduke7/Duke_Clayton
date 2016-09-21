98bd2e8f94cd2ed26541d13927c813fdc2d129b7
cbduke7 <cbduke14@yahoo.com>
2016-09-21 10:25:24 -0700

    finished and added files

--- PyLesson_04/Ex_01Receipt.py
+++ PyLesson_04/Ex_01Receipt.py
@@ -1,5 +1,5 @@
 def format(item, price):
-    print("{*:<1}{:^15}\t{:10.2f}".format(item, price))
+    print("*{:^15}.....\t{:7.2f}".format(item, price))
 
 item1 = input("Please enter item 1:")
 price1 = float(input("Please enter the price:"))
@@ -12,10 +12,12 @@ subtotal = (price1 + price2 + price3)
 tax = (subtotal * .075)
 total = (subtotal + tax)
 
-print("<<<<<<<receipt>>>>>>")
+print("<<<<<<<<<<__Receipt__>>>>>>>>>>")
 format(item1, price1)
 format(item2, price2)
 format(item3, price3)
 format("Subtotal: ", subtotal)
-format("Tax: ")
-format("Total: ")
+format("Tax: ", tax)
+format("Total: ", total)
+print("_______________________________")
+print( "* Thank you and come again *")
---
+++ PyLesson_04/Ex_02IdCard.py
@@ -0,0 +1,14 @@
+def format(left,right):
+    print("* {:^12}{:>13} *".format(left, right))
+    
+left2 = input("Enter your first name: ")
+right2 = input("Enter your last name: ")
+left3 = input("Enter your title: ")
+left1 = input("Enter your school site: ")
+right1 = input("Enter the school year: ")
+right3 = input("What is your subject? ")
+
+print("
+format(left1, right1)
+format(left2, right2)
+format(left3, right3)
