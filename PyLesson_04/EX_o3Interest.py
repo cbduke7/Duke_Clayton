1fc0177d114c15b6e8bcdd6f6cb6d6e5f38da57d
cbduke7 <cbduke14@yahoo.com>
2016-09-29 10:26:08 -0700

    added files

--- PyLesson_04/EX_o3Interest.py
+++ PyLesson_04/EX_o3Interest.py
@@ -1,11 +1,12 @@
 def interest(r, p, n, t):
-    return ((r / n + 1) ** (n * t) * p)
-
-r = float(input("What is your interest rate as a deimal? "))
+    return ((r/100 / n + 1) ** (n * t) * p)
+    
+    
+r = float(input("What is your interest rate? "))
 p = int(input("What is your principal? "))
 n = int(input("How many times is the loan compounded per year? "))
 t = int(input("What is the life of the loan in years? "))
 
 
 
-print("The total cost of the loan is $",interest(r, p, n, t))
+print("The total cost of the loan is $ {:.2f}".format(interest(r, p, n, t)), ".")
---
+++ PyLesson_04/rectangle.py
@@ -0,0 +1,5 @@
+length = int(input("Please enter the length of the rectangle: "))
+width = int(input("Please enter the width of the rectangle: "))
+perimter
+
+calcPerim = (l * w * 2)
