64b685b4a92c582534478eefc665de0fce5fb7e8
cbduke7 <DukeC9804@sduhsd.lan>
2016-10-11 10:13:39 -0700

    added and finished files

--- PyLesson-05/Ex_02Receipt.py
+++ PyLesson-05/Ex_02Receipt.py
@@ -1,7 +1,8 @@
 def format(item, price):
     print("*{:^15}.....\t{:7.2f}".format(item, price))
 
-def discount():
+def setDiscount():
+    global discount
     if subtotal >= 2000:
         discount = 15
     if subtotal < 2000:
@@ -14,11 +15,10 @@ price2 = float(input("Please enter the price:"))
 item3 = input("Please enter item 3:")
 price3 = float(input("Please enter the price:"))
 
-
 subtotal = (price1 + price2 + price3)
-discount()
-disamount = (subtotal * (100 * discount))
-tax = (subtotal * .075)
+setDiscount()
+disamount = (subtotal * (discount / 100))
+tax = ( subtotal * .075)
 total = (subtotal + tax - disamount)
 
 
@@ -27,7 +27,7 @@ format(item1, price1)
 format(item2, price2)
 format(item3, price3)
 format("Subtotal: ", subtotal)
-format("Discount: ", discount,"%")
+format("Discount: ", discount)
 format("Tax: ", tax)
 format("Total: ", total)
 print("_______________________________")
