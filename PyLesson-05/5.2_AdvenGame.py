81340f1190276a7350b789255638660d370450b9
cbduke7 <cbduke14@yahoo.com>
2016-10-25 10:26:07 -0700

    completed lesson 5

--- PyLesson-05/5.2_AdvenGame.py
+++ PyLesson-05/5.2_AdvenGame.py
@@ -1,9 +1,34 @@
-choice = input("Would you like to explore the ocean or the a forest? ")
+choice = input("Would you like to live in the ocean or the a forest? ")
 
 if choice == "forest":
-    a == input("Do you want to explore the dark cave or steal the kings crown? ")
-        if a == "dark cave"
-        
-        
+    story1 = input("Are you a commoner or a knight? ")
+    if story1 == "commoner": 
+        a = input("Do you want to explore a dark cave or steal the kings crown?  ")
+        if a == "dark cave" or "explore the dark cave":
+            print("You travel into the cave and fight off monsters. \n After hours you arrise from the cave a hero.")
+        else:
+            print("You attempt to stal the crown but \n are caught by the king's gaurds and thrown into the palaces prison.")
+    else:
+        b = input("Do you want to participate in a jousting tournament or rescue a damsial in distress")
+        if b == "participate in a jousting tournameny" or "jousting tournament":
+            print("You make it to the finals of the tournament but \n you get knocked off your horse and lose.")
+        else:
+            print("Trolls attempt to kidnap a princess. \n You run over fight off the trolls and save the girl. \n You are then married to her by the king.")
+else:
+    story2 = input("Are you a mermaid or a clown fish? ")
+    if story2 == "mermaid":
+        c = input("Do you want to be a super hero or a the king of the sea? ")
+        if c == "super hero":
+            print("You are a super hero named merman. \n You use your powers to create a whirl pool and \n defeat your arch enemies', Rayman and The Dirty Bubble.")
+        else:
+            print("Your name is king neptune and you rule the sea. \n Youparticipate in a ham burger making competion. \n You lose to Spongebob, a local fry cook from the Krusty Krab.")
+    else:
+        d = input("Do you want to be a father or a son")
+        if d == "father":
+            print("Your son is taken by scuba divers. \n You search all across the ocean searching for your son meeting \n turtles, sharks, a whale, and a few types of fishes. \n You finially find your son.")
+        else:
+            print("You are taken from your home while trying to touch a but. \n You live in a fish tank with other fish. \n You and the others try to escape but only you end up escaping. \n You reunite with your father.")
+                
+                  
     
     
