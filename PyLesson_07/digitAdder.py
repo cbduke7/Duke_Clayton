38bbf9f8562ed48957827a084242f2f933642456
cbduke7 <cbduke14@yahoo.com>
2016-11-28 10:25:25 -0800

    lesson 7

---
+++ PyLesson_07/digitAdder.py
@@ -0,0 +1,11 @@
+number = int(input("Please enter a number:" )
+total = 0
+num = number
+
+sumDigits():
+    while number > 0:
+        total += number % 10
+        number = (num / 10)
+
+sumDigits()
+print("The sum of the digits in", number,"is", total)
