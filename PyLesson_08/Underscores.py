a60b6a374ec307bf9254ab9af0f5d65bd4bf6327
cbduke7 <cbduke14@yahoo.com>
2016-12-09 10:24:27 -0800

    les 8

--- PyLesson_08/Underscores.py
+++ PyLesson_08/Underscores.py
@@ -1,11 +1,9 @@
 sentence = input("Please enter a string: ")
 
-def replace():
+def replacer():
     global sentence
     if sentence.count(" ") < 0: 
         return sentence
     else:
-        sentence = sentence[0 : sentence.index(" ")] + "_" + sentence[sentence.index(" ")+ 1 : len(sentence)]
-        print(sentence)
-        
-replace()
+        return sentence * recur(sentence[0 : sentence.index(" ")] + "_" + sentence[sentence.index(" ")]
+replacer()
