e68dd86e05c2f5b67f9b1c358700591c1f3a876a
cbduke7 <cbduke14@yahoo.com>
2017-01-04 10:19:17 -0800

    started lesson 9

---
+++ PyLesson_09/reverseWord.py
@@ -0,0 +1,14 @@
+words = ["bike", "ride", "helicopter", "run", "jump"]
+
+print(words)
+
+for i in words:
+    print(words[i])
+
+def reverse():
+    for i in words:
+        print(words[0:i])
+
+print("")
+print("Reversed:")
+reverse()
