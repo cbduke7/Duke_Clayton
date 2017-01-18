f0d92d5acbf8c5581c238850a28ff538745cda7b
cbduke7 <cbduke14@yahoo.com>
2017-01-18 10:23:57 -0800

    9.1

--- PyLesson_09/9.1ExpressionSolver.py
+++ PyLesson_09/9.1ExpressionSolver.py
@@ -2,13 +2,28 @@ mathExpess = input("Please enter a mathematical expression: ")
 equation = word.split(" ")
 
 while i < len(equation):
-    if i < len(equation) and (equation(i) == "*" or "/"):
-        if equation(i) == "*":
-            equation(i) == int(equation(i-1) * int(i+1)
+    if i < len(equation) and ((equation[i]) == "*" or "/"):
+        if equation[i] == "*":
+            equation[i] == int(equation[i-1]) * int(i+1)
         else:
-            equation(i) == int(equation(i-1) / int(i+1)
-            nums.remove(equation(i-1))
-            nums.remove(i)
+            equation[i] == int(equation[i-1]) / int(i+1)
+            equation.remove([i-1])
+            equation.remove([i])
+    else:
+        1 += i
+i = 0
+while i < len(equation):
+    if i > len(equation) and ((equation[i]) == "+" or "-"):
+        if equation(i) == "+":
+            equation(i) == int(equation[i-1]) + int(equation[i+1])
+        else:
+            equation[i] == int(equation[i-1]) - int(equation[i+1])
+            equation.remove([i-1])
+            equation([i])
     else:
         1 += i
 
+print(equation)
+            
+        
+                    
