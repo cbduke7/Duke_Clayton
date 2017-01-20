a0c120c0d62672ed54c933a6810254eb90277e45
cbduke7 <cbduke14@yahoo.com>
2017-01-20 10:24:40 -0800

    les 9 and 10

--- PyLesson_09/fibonacciSequence.py
+++ PyLesson_09/fibonacciSequence.py
@@ -1,6 +1,11 @@
 startNum = int(input("Please enter your starting number: "))
 seqSize = int(input("Please enter your sequence size: "))
+seq = []*seqSize
 
-seq = [
+for i in range(0, seqSize):
+    if i == 0 or 1:
+        seq[i] == startNum
+    else:
+        seq[i] = seq[index - 2]
 
-for 
+print(seq) 
