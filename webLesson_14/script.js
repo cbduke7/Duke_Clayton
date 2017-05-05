13bd6252189616131e4cb85fd167dd3249c3fdd7
cbduke7 <cbduke14@yahoo.com>
2017-05-05 10:25:36 -0700

    worked on 14

--- webLesson_14/script.js
+++ webLesson_14/script.js
@@ -1,24 +1,28 @@
 function validate() {
 	var x = document.forms.input.username.value;
-	var y = document.forms.input.password.value;
 	var atPos = x.indexOF("@")
 	var dotPos = x.lastIndexOf(".");
+	
+
+	if(atPos < 1 || dotPos < atPos+2 || dotPos + 2 > x.length)
+		alert("We are unable to process your request. Please correct the following errors...This is not a valid email address!");
+	
+	else
+		alert("Success!")
+}
+
+function validate() {
+	var y = document.forms.input.passwd.value;
+	
+	if(y.length < 6)
+		alert("Your password does not meet the mininum requirements")
+	
+	else
+		alert("Success!")
+}
 
 	
 	
-	if(atPos < 1)
-		alert("This is not a valid email address!");
-	else
-		alert("Success!")
-	if(dotPos < atPos+2)
-		alert("This is not a valid email address!");
-	else
-		alert("Success!")
-	if(dotPos + 2 > x.length)
-		alert("This is not a valid email address!");
-	else
-		alert("Success!")
-	if(y.length > 6)
-		alert
-		
-}
+	
+
+
