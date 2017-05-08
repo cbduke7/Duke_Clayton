41e2b41bec12e6f6807e0b0a65dd17951ddfeb7e
cbduke7 <cbduke14@yahoo.com>
2017-05-08 10:25:38 -0700

    14

--- webLesson_14/script.js
+++ webLesson_14/script.js
@@ -1,27 +1,34 @@
-function validate() {
-	var x = document.forms.input.username.value;
-	var atPos = x.indexOF("@")
-	var dotPos = x.lastIndexOf(".");
+function validateEmail() {
+	var atPos = user.indexOF("@");
+	var dotPos = user.lastIndexOf(".");
 	
 
-	if(atPos < 1 || dotPos < atPos+2 || dotPos + 2 > x.length)
-		alert("We are unable to process your request. Please correct the following errors...This is not a valid email address!");
+	if(atPos < 1 || dotPos < atPos + 2 || dotPos + 2 > user.length)
+		alert("We are unable to process your request. Please correct the following errors... This is not a valid email address!");
 	
 	else
-		alert("Success!")
+		alert("Success!");
+}
+
+
+function validatePass() {
+	if(pass.length < 6)
+		alert("We are unable to process your request. Please correct the following errors... Your password does not meet the mininum requirements");
+	
+	else
+		alert("Success!");
 }
 
 function validate() {
-	var y = document.forms.input.passwd.value;
+	var atPos = user.indexOF("@");
+	var dotPos = user.lastIndexOf(".");
 	
-	if(y.length < 6)
-		alert("Your password does not meet the mininum requirements")
+	if(atPos < 1 || dotPos < atPos+2 || dotPos + 2 > user.length || pass.length < 6)
+		alert("We are unable to process your request. Please correct the following errors... You did not provide a valid email address and your password does not meet the mininum requirements");
 	
 	else
-		alert("Success!")
-}
-
-	
+		alert("Success");
+}	
 	
 	
 
